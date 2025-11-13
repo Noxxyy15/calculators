8c7cd4fad30808a41f19247ba5d46ceced3c4753
Nox <Noxxyy15@proton.me>
2025-11-13 20:26:59 +0000

    Implement basic calculator functionality
    Added a basic calculator that performs addition, subtraction, multiplication, and division based on user input.

--- calc.go
+++ calc.go
@@ -2,5 +2,39 @@ package main
 import ("fmt")
 
 func main() {
-  fmt.Println("Hello World!")
+  fmt.Println("what operation do you want to do? (1=+ 2=- 3=* 4=/)")
+  var MathType int
+  var num1 int
+  var num2 int
+  var ans int
+  fmt.Scan(&MathType)
+  if MathType == 1 {
+    fmt.Println("what the first number?")
+    fmt.Scan(&num1)
+    fmt.Println("whats the number you want to add to that?")
+    fmt.Scan(&num2)
+    ans = num1 + num2
+    fmt.Println("the answer is: ", ans)
+  } else if MathType == 2 {
+    fmt.Println("what the first number?")
+    fmt.Scan(&num1)
+    fmt.Println("whats the number you want to subtract to that?")
+    fmt.Scan(&num2)
+    ans = num1 - num2
+    fmt.Println("the answer is: ", ans)
+  } else if MathType == 3 {
+    fmt.Println("what the first number?")
+    fmt.Scan(&num1)
+    fmt.Println("whats the number you want to times by?")
+    fmt.Scan(&num2)
+    ans = num1 * num2
+    fmt.Println("the answer is: ", ans)
+  } else if MathType == 4 {
+    fmt.Println("what the first number?")
+    fmt.Scan(&num1)
+    fmt.Println("whats the number you want to devide by?")
+    fmt.Scan(&num2)
+    ans = num1 / num2
+    fmt.Println("the answer is: ", ans)
+  }
 }
